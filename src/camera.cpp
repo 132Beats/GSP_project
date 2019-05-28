ff1f83289da5ecc5c7a722a4a99d9520b3807f53
Hendrik <Henni.KleinH@gmail.com>
2019-05-28 17:30:07 +0200

    cam files++

---
+++ src/camera.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hendrik on 28.05.19.
+//
+
+#include "camera.h"
---
+++ src/camera.h
@@ -0,0 +1,8 @@
+//
+// Created by hendrik on 28.05.19.
+//
+
+#ifndef GSP1_CAMERA_H
+#define GSP1_CAMERA_H
+
+#endif //GSP1_CAMERA_H
